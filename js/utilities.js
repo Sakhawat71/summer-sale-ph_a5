9a82010fccafd5de1b04e389c873d4aaf5b3e328
sakhawat71 <sakhawat.showrav@gmail.com>
2023-10-28 20:12:40 +0600

    debug

--- js/utilities.js
+++ js/utilities.js
@@ -1,14 +1,3 @@
-// const purchaseBtn = () =>{
-//     const purchBtn = document.getElementById("purchase-btn");
-
-//     if(sumTotalPrice > 0){
-//         purchBtn.removeAttribute("disabled");
-//         return;
-//     }
-//     else{
-//         purchBtn.setAttribute("disabled",true);
-//     }
-// }
 
 const completePurchase = () =>{
     window.location = "index.html"
